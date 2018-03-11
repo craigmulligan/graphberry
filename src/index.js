85f06f3e40c8252ab823b6af290f5c4913149116
Craig Mulligan <hobochildster@gmail.com>
2018-03-11 16:04:26 +0000

    commit add wireless-tools

--- src/index.js
+++ src/index.js
@@ -1,34 +1,39 @@
 const { GraphQLServer, PubSub } = require('graphql-yoga')
 const ble = require('./ble')
 const wireless = require('./wireless-tools')
-
+const loader = require('./loader')
 const PORT = process.env.PORT || 4000
 
-const { prepare } = require('@gramps/gramps')
+const boot = async () => {
+  const dataSources = await loader()
+  const { prepare } = require('@gramps/gramps')
 
-// merge all data sources to a single schema
-const opts = prepare({ dataSources: [ble, wireless] })
+  // merge all data sources to a single schema
+  const opts = prepare({ dataSources })
 
-const pubsub = new PubSub()
-const server = new GraphQLServer({
-  ...opts,
-  context: ({ request, connection }) => ({
-    request,
-    connection,
-    pubsub,
-  }),
-})
+  const pubsub = new PubSub()
+  const server = new GraphQLServer({
+    ...opts,
+    context: ({ request, connection }) => ({
+      request,
+      connection,
+      pubsub,
+    }),
+  })
 
-server.start(
-  {
-    subscriptions: {
-      onConnect: (connectionParams, websocket) => {
-        return {
-          websocket,
-        }
+  server.start(
+    {
+      subscriptions: {
+        onConnect: (connectionParams, websocket) => {
+          return {
+            websocket,
+          }
+        },
       },
+      port: PORT,
     },
-    port: PORT,
-  },
-  () => console.log('Server is running on localhost:4000'),
-)
+    () => console.log('Server is running on localhost:4000'),
+  )
+}
+
+boot()
