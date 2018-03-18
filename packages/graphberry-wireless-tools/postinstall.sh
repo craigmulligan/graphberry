7087b50154bb6716c25a798b49e1e15a0f4a1801
Craig Mulligan <hobochildster@gmail.com>
2018-03-18 17:41:49 +0000

    improve postinstall scripts

--- packages/graphberry-wireless-tools/postinstall.sh
+++ packages/graphberry-wireless-tools/postinstall.sh
@@ -1,14 +1,14 @@
 red=`tput setaf 1`
 reset=`tput sgr0`
 if hash apt-get 2>/dev/null; then
-  apt-get install bluetooth bluez libbluetooth-dev libudev-dev 2>/dev/null
+  apt-get install network-tools 2>/dev/null
   if [ $? -eq 0 ]; then
       echo Successfully installed network-tools 
   else
       echo "**********************************"
-      echo ${red}Failed to install ble dependencies${reset}
+      echo ${red}Failed to install network-tools${reset} 
       echo try running:
-      echo "$ sudo apt-get update && apt-get install bluetooth bluez libbluetooth-dev libudev-dev "
+      echo "$ apt-get update && apt-get install network-tools"
       echo "**********************************"
   fi
 else
